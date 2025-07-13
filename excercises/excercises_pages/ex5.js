ab9e1bfac62e2230868c01af2fb9ce4cc4b28b01
Cybergman-droid <jasongakpo20@gmail.com>
2025-07-13 21:42:03 +0100

    Implement number guessing game logic in ex5.js
    Added event listener and check function to handle user guesses between 1 and 10. The script now provides feedback for correct and incorrect guesses, replacing previous placeholder code.

--- excercises/excercises_pages/ex5.js
+++ excercises/excercises_pages/ex5.js
@@ -1,2 +1,13 @@
-let randnum = Math.floor(Math.random() * 50) + 50;
-console.log(randnum);s
+let randnum = Math.floor((Math.random() * 10) + 1);
+let submit = document.getElementById('check');
+
+function check() {
+    let userguess = document.getElementById('guess').value;
+    if (userguess == randnum) {
+        document.getElementById('result').innerHTML = 'You guessed it! Well done!';
+    } else {
+        document.getElementById('result').innerHTML = 'Try again';
+    }
+}
+
+submit.addEventListener('click', check);
