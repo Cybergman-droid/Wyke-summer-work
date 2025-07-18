c5f24de46d41909dda154bd2fa8bd46aac4dd406
Cybergman-droid <jasongakpo20@gmail.com>
2025-07-18 20:01:57 +0100

    Add password check input and logic to Exercise 6
    Introduced a password input field, check button, and result display to ex6.html. Implemented basic password checking logic in ex6.js that updates the result based on user input.

---
+++ excercises/excercises_pages/ex6.js
@@ -0,0 +1,18 @@
+let password = "password";
+let input = document.getElementById("guess");
+let checkButton = document.getElementById("check");
+let result = document.getElementById("result");
+
+function checkPassword() {
+    let userGuess = input.value;
+    do {
+        if (userGuess == password) {
+            result.value = "Correct!";
+        } else {
+            result.value = "Incorrect!";
+        }
+    }
+    while (userGuess != password);
+}
+
+checkButton.addEventListener("click", checkPassword);
