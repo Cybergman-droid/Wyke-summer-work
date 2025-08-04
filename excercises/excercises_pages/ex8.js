3dfa52271ea53610d746d7003e572b82a162a923
Cybergman-droid <jasongakpo20@gmail.com>
2025-08-05 00:56:32 +0100

    Improve countdown functionality and UX
    Refactored countdown to display all numbers and 'High five' if divisible by 5. Added support for triggering countdown with Enter key and improved output rendering.

--- excercises/excercises_pages/ex8.js
+++ excercises/excercises_pages/ex8.js
@@ -1,14 +1,25 @@
-let num = document.getElementById('number').value;
+let numInput = document.getElementById('number');
 let submit = document.getElementById('submit');
 let output = document.getElementById('result')
 
 function countdown() {
+    let num = Number(numInput.value)
+    output.innerHTML = ''
     for(let i = num; i != 0; i--){
-        output.innerHTML = i
-        console.log(i)
+        output.innerHTML += (`${i} <br>`)
+        console.log(i);
+    }
 
+    if(num % 5 === 0){
+        output.innerHTML += (`High five <br>`)
     }
 
 }
 
-submit.addEventListener('click',countdown);
+submit.addEventListener('click',countdown)
+
+numInput.addEventListener('keyup',function(event){
+    if(event.key === 'Enter'){
+        countdown();
+    }
+})
